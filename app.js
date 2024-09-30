548ce055177158e83b9f47ebf439d93cbe13bf5b
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2024-09-30 14:32:54 +0530

    form-alignments

---
+++ app.js
@@ -0,0 +1,7 @@
+const signIn = document.getElementById('signInUp');
+const removeName  = document.getElementById('removeName');
+const signText = document.getElementById('signOption');
+function signIn(){
+    document.signIn.innerHTML = "Sign In";
+    
+}
